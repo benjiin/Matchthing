e2c2aaceca1059cc975e109af589f5fd20fa0a98
Benjamin Rosenkranz <benjamin.rosenkranz@gmail.com>
2017-04-22 12:22:36 +0200

    Projektdateien hinzufügen.

---
+++ WpfApp1/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Game
+    {
+        public void game()
+        {
+            Human player1 = new Human("Spieler1");
+            Human player2 = new Human("Spieler2");
+            Human player3 = new Human("Spieler3");
+            Human player4 = new Human("Spieler4");
+            Human player5 = new Human("Spieler5");
+            Human player6 = new Human("Spieler6");
+
+        }
+    }
+}
