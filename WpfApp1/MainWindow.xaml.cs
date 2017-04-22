477cd97adbef019b122b063d42d8a2027234730e
Benjamin Rosenkranz <benjamin.rosenkranz@gmail.com>
2017-04-22 20:39:08 +0200

    2 Spieler Modus aktiviert

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -32,17 +32,28 @@ namespace WpfApp1
             get { return amountMatches; }
             set { amountMatches = value; }
         }
+        private int round;
+
+        public int Round
+        {
+            get { return round; }
+            set { round = value; }
+        }
+
+
 
 
         public void Button_Click(object sender, RoutedEventArgs e)
         {
             textblock1.Text = "";
-
+            textblock2.Text = "";
+            textblock3.Text = "";
             AmountMatches = 21;
+            round = 1;
 
 
             Button match1 = new Button();
-            match1.Content = "I";
+            match1.Content = " 1 ";
             match1.Height = button1.Height;
             match1.Width = button1.Width;
             match1.HorizontalAlignment = button1.HorizontalAlignment;
@@ -50,7 +61,7 @@ namespace WpfApp1
             match1.Margin = button1.Margin;
 
             Button match2 = new Button();
-            match2.Content = "II";
+            match2.Content = " 2 ";
             match2.Height = button3.Height;
             match2.Width = button3.Width;
             match2.HorizontalAlignment = button3.HorizontalAlignment;
@@ -58,7 +69,7 @@ namespace WpfApp1
             match2.Margin = button3.Margin;
 
             Button match3 = new Button();
-            match3.Content = "III";
+            match3.Content = " 3 ";
             match3.Height = button5.Height;
             match3.Width = button5.Width;
             match3.HorizontalAlignment = button5.HorizontalAlignment;
@@ -83,67 +94,121 @@ namespace WpfApp1
             match2.Click += Match2_Click;
             match3.Click += Match3_Click;
 
-            textblock1.Text = amountMatches.ToString();
+            textblock3.Text = "Runde: " + round;
 
-        }
+            textblock2.Text = "Noch " + amountMatches.ToString() + " Streichhölzer übrig";
 
-        private void Match3_Click(object sender, RoutedEventArgs e)
-        {   
-            if(AmountMatches>=3)
+            if (round % 2 == 0)
             {
-                amountMatches -= 3;
-                textblock1.Text = amountMatches.ToString();
+                textblock1.Text = "Spieler 2 ist an der Reihe";
             }
-            if (AmountMatches <= 0)
+            else
             {
-                textblock1.Text = "Du hast verloren";
+                textblock1.Text = "Spieler 1 ist an der Reihe";
             }
-        }
 
-        private void Match2_Click(object sender, RoutedEventArgs e)
-        {
-            if(AmountMatches>=2)
-            {
-                amountMatches -= 2;
-                textblock1.Text = amountMatches.ToString();
-            }
-            if (AmountMatches <= 0)
-            {
-                textblock1.Text = "Du hast verloren";
-            }
         }
 
         private void Match1_Click(object sender, RoutedEventArgs e)
         {
-            if(AmountMatches>=1)
+            if (AmountMatches>=1)
             {
                 amountMatches -= 1;
-                textblock1.Text = amountMatches.ToString();
+                round += 1;
+                textblock2.Text = "Noch " + amountMatches.ToString() + " Streichhölzer übrig";
             }
-            if (AmountMatches <= 0)
+            if (round % 2 == 0)
             {
-                textblock1.Text = "Du hast verloren";
-            } 
+                textblock1.Text = "Spieler 2 ist an der Reihe";
+                if(AmountMatches <= 0)
+                {
+                    textblock1.Text = "Spieler 2. Du hast verloren";
+                }
+            }
+            else
+            {
+                textblock1.Text = "Spieler 1 ist an der Reihe";
+                if (AmountMatches <= 0)
+                {
+                    textblock1.Text = "Spieler 2. Du hast verloren";
+                }
+            }
+            textblock3.Text = "Runde: " + round;
+
+        }
+
+        private void Match2_Click(object sender, RoutedEventArgs e)
+        {
+            if (AmountMatches >= 2)
+            {
+                amountMatches -= 2;
+                round += 1;
+                textblock2.Text = "Noch " + amountMatches.ToString() + " Streichhölzer übrig";
+            }
+            if (round % 2 == 0)
+            {
+                textblock1.Text = "Spieler 2 ist an der Reihe";
+                if (AmountMatches <= 0)
+                {
+                    textblock1.Text = "Spieler 2. Du hast verloren";
+                }
+            }
+            else
+            {
+                textblock1.Text = "Spieler 1 ist an der Reihe";
+                if (AmountMatches <= 0)
+                {
+                    textblock1.Text = "Spieler 2. Du hast verloren";
+                }
+            }
+            textblock3.Text = "Runde: " + round;
+        }
+
+        private void Match3_Click(object sender, RoutedEventArgs e)
+        {
+            if (AmountMatches >= 3)
+            {
+                amountMatches -= 3;
+                round += 1;
+                textblock2.Text = "Noch " + amountMatches.ToString() + " Streichhölzer übrig";
+            }
+            if (round % 2 == 0)
+            {
+                textblock1.Text = "Spieler 2 ist an der Reihe";
+                if (AmountMatches <= 0)
+                {
+                    textblock1.Text = "Spieler 2. Du hast verloren";
+                }
+            }
+            else
+            {
+                textblock1.Text = "Spieler 1 ist an der Reihe";
+                if (AmountMatches <= 0)
+                {
+                    textblock1.Text = "Spieler 2. Du hast verloren";
+                }
+            }
+            textblock3.Text = "Runde: " + round;
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            textblock3.Text = "In development";
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-
+            textblock3.Text = "In development";
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-
+            textblock3.Text = "In development";
         }
 
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
-
+            textblock3.Text = "In development";
         }
     }
 
